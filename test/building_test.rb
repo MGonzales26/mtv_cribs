75a4bcf09aff999ea4d64bdc3ab6cb5825dbd26d
Megan Campbell <megan.campbell26@yahoo.com>
2020-10-20 09:26:34 -0600

    Add add unit test

--- test/building_test.rb
+++ test/building_test.rb
@@ -17,4 +17,18 @@ class BuildingTest < Minitest::Test
 
     assert_equal [], building.units
   end
+
+  def test_it_can_add_apartments
+    building = Building.new
+
+    unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
+    unit2 = Apartment.new({number: "B2", monthly_rent: 999, bathrooms: 2, bedrooms: 2})
+
+    building.add_unit(unit1)
+    building.add_unit(unit2)
+
+    expected = [unit1, unit2]
+
+    assert_equal expected, building.units
+  end
 end
